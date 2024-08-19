f6b784ccf9cbaa9af2f5dfeb1599c52fe711cb59
Coultags <167041164+Coultags@users.noreply.github.com>
2024-08-19 02:03:29 -0600

    Add files via upload
    Created a DataBase to add one student

---
+++ C# Part 2/Program.cs/Program.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDB
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Jughead" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.WriteLine("Student has been saved successfully!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ C# Part 2/Program.cs/Program.cs/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.cs
+{
+    class StudentContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public Dbset<Student> Students { get; set; }
+        public Dbset<Grade> Grades { get; set; }
+    }
+}
